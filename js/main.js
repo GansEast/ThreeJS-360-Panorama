3dc31b1ae434a8e91e5fb2ccf6e60ef379e2ae9c
GansEast <ganseast@gmail.com>
2021-02-20 15:57:03 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ var camera,
         onMouseDownMouseY = 0,
         onMouseDownLon = 0,
         onMouseDownLat = 0,
-        width = 1920, // int || window.innerWidth
-        height = 1080, // int || window.innerHeight
+        width = 1440, // int || window.innerWidth
+        height = 650, // int || window.innerHeight
         ratio = width / height;
 var texture = THREE.ImageUtils.loadTexture('img/spherical_texture.jpg', new THREE.UVMapping(), function() {
     init();
